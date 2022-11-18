ddd44da26018a1abd5c441b2ba8b9ee7070ae95a
DiegoArcelli <diegoarcelli99@gmail.com>
2022-11-18 22:55:06 +0100

    skeleton code for the reflections assignment

--- Assignment1/Exercise2/XMLSerializer/src/xmlserializer/XMLSerializer.java
+++ Assignment1/Exercise2/XMLSerializer/src/xmlserializer/XMLSerializer.java
@@ -1,20 +1,70 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package xmlserializer;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 /**
  *
  * @author diego
  */
 public class XMLSerializer {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    private Object objects[];
+    
+    public XMLSerializer() {
+        this.objects = null;
     }
+    
+    public XMLSerializer(Object objects[]) {
+        this.objects = objects;
+    }
+    
+    public void serialize(Object objects[]) {
+        for (Object obj : objects) {
+            Class obj_class = obj.getClass();
+            
+            Method[] methods = obj_class.getMethods();
+            for (Method method : methods) {
+                String return_type = method.getReturnType().getName();
+                String method_name = method.getName();
+                
+                Parameter[] parameters = method.getParameters();
+                for (Parameter parameter : parameters) {
+                    String param_type = parameter.getType().getName();
+                    String param_name = parameter.getName();
+                    
+                }
+                
+            }
+            
+            Field[] fields = obj_class.getFields();
+            for (Field field : fields) {
+                String field_type = field.getType().getName();
+                String field_name = field.getName();
+            }
+            
+            Constructor[] constructors = obj_class.getConstructors();
+            for (Constructor constructor : constructors) {
+                String constructor_name = constructor.getName();
+                Parameter[] parameters = constructor.getParameters();
+                for (Parameter parameter : parameters) {
+                    String param_type = parameter.getType().getName();
+                    String param_name = parameter.getName();
+                }
+            }
+            
+            
+            
+        }
+    }
+    
+    
+    
     
 }
