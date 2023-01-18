35e3dcf9c0ac0aa7f002785c57c16fb6b84e2b2c
DiegoArcelli <diegoarcelli99@gmail.com>
2023-01-18 17:49:04 +0100

    commented code of haskell assignment

--- Assignment2/Exercises_1_2/MultiSet.hs
+++ Assignment2/Exercises_1_2/MultiSet.hs
@@ -15,31 +15,45 @@ empty :: MSet a
 empty = MS []
 
 data MSet a = MS [(a, Int)] deriving (Show)
--- mset1 = MS [(1, 10), (2, 15), (3, 7), (4, 3)]
--- mset2 = MS [(1, 10), (2, 15), (3, 7), (4, 5), (10, 7)]
 
-
+-- extract the list from the MSet
 getList :: MSet a -> [(a, Int)]
 getList (MS x) = x
 
+{-- 
+recursively scan the MSet to check if the elment is already present in MSet.
+If the element is found its multiplicity is increased by 1 and the recursion is stopped.
+If the element is not in the MSet it is added with multilplicity 1
+--}
 add :: Eq a => MSet a -> a -> MSet a
-add (MS (x:xs)) v = if (v == fst x)
-    then MS ( [(fst x, snd x + 1)] ++ xs )
-    else MS ( [(fst x, snd x)] ++ (getList (add (MS xs) v)))
+add (MS ((e, c):xs)) v = if (v == e)
+    then MS ( [(e, c + 1)] ++ xs )
+    else MS ( [(e, c)] ++ (getList (add (MS xs) v)))
 add (MS []) v = MS [(v, 1)]
 
 
+{-- 
+recursively scan the list of the MSet, if the first element of the pair (value, multiplicity)
+is equal to the passed value v, then the second element of the pair is returned, 
+otherwise 0 is returned at the end
+--}
 occs :: Eq a => MSet a -> a -> Int
-occs (MS (x:xs)) v = (if v == fst x then snd x else 0) + (occs (MS xs) v)
+occs (MS ((e, c):xs)) v = (if v == e then c else (occs (MS xs) v)) 
 occs (MS []) _ = 0
 
-
+{--
+apply the map combinator to the list of the MSet returing
+a list containing only the first element of each pair
+--}
 elems :: MSet a -> [a]
-elems (MS (x:xs)) = [fst x] ++ elems (MS xs)
-elems (MS []) = []
+elems (MS list) = map (\(e, c) -> e) list
 
-
-
+{--
+function to check if an element is present in the MSet with a given multilplicity.
+The list of the MSet is scanned recursively and if one of the pair (value, multiplicity)
+is equal to the passed pair then True is returned and the recusion is stopped,
+otherwise False is returned
+--}
 isIn :: Eq a => (a, Int) -> MSet a -> Bool
 isIn (v, n) (MS (x:xs)) = if ((v,n) == x)
     then True
@@ -47,6 +61,12 @@ isIn (v, n) (MS (x:xs)) = if ((v,n) == x)
 isIn (v, n) (MS []) = False
 
 
+{--
+recursively scan the list of the first MSet and check if an element with its mutiplicity 
+is present also in the secon MSet (using the isIn function). If an pair of the first MSet
+is not present in the second MSet then the recusion is stopped and False is returned,
+otherwise True at the end.
+--}
 subeq :: Eq a => MSet a -> MSet a -> Bool
 subeq (MS (x:xs)) mset2 = if (isIn x mset2)
     then subeq (MS xs) mset2
@@ -54,30 +74,76 @@ subeq (MS (x:xs)) mset2 = if (isIn x mset2)
 subeq (MS []) mset2 = True
 
 
+
+{--
+function to add an element v to the MSet but with a given mulitplicity n. 
+The function works like the add function but in this case if the element is found instead
+of increasing the multiplicity by 1, it is increased by n. If the element is not found 
+is added to the MSet with mutiplicity n
+--}
 addMult :: Eq a => MSet a -> (a, Int) -> MSet a
-addMult (MS (x:xs)) (v, n) = if (v == fst x)
-    then MS ( [(fst x, snd x + n)] ++ xs )
-    else MS ( [(fst x, snd x)] ++ (getList (addMult (MS xs) (v, n))))
+addMult (MS ((e, c):xs)) (v, n) = if (v == e)
+    then MS ( [(e, c + n)] ++ xs )
+    else MS ( [(e, c)] ++ (getList (addMult (MS xs) (v, n))))
 addMult (MS []) (v, n) = MS [(v, n)]
 
 
+{--
+Function that implements the union of two MSets.
+The foldr scan all the pairs (element, multiplicity) of the first MSet
+and it adds them to the second MSet (which is the starting value of the accumulator)
+using the function addMult
+--}
 union :: Eq a => MSet a -> MSet a -> MSet a
-union (MS (x:xs)) mset2 = union (MS xs) (addMult mset2 x)
-union (MS []) mset2 = mset2
+union (MS list) mset = foldr (\x acc -> addMult acc x) mset list
 
+
+
+{--
+Foldr is implemented for MSet like it would be implemented for list, but only the first element
+of each pair of the MSet is considered when the binary function f is applied to the
+current value of the accumulator and the element of the MSet
+--} 
+
+
+foldrMSet :: (a -> b -> b) -> b -> MSet a -> b
+foldrMSet f acc (MS []) = acc
+foldrMSet f acc (MS ((e, c):xs)) = f e (foldr f acc (MS xs))
 
 instance Foldable MSet where
-    foldr f acc (MS []) = acc
-    foldr f acc (MS (x:xs)) = f (fst x) (foldr f acc (MS xs))
+    foldr f acc mset = foldrMSet f acc mset
 
 
+{--
+The equality two MSets is checked verifying verifying if each element of mset1 is also an
+element of mset2 with the same multiplicity and also the viceversa. 
+This is achived using the previously defined subeq function in both directions
+--}
 instance Eq a => Eq (MSet a) where
     (==) mset1 mset2 = (subeq mset1 mset2) && (subeq mset2 mset1)
 
 
-mapMSetRec :: (a -> b) -> MSet a -> MSet b
-mapMSetRec f (MS (x:xs)) = MS ( [ (f (fst x), snd x)] ++ (getList (mapMSetRec f (MS xs))) )
-mapMSetRec f (MS []) = MS []
 
+{--
+function which applies a function f to the first element of each pair of the MSet
+--}
+mapMSetRec :: (a -> b) -> MSet a -> MSet b
+mapMSetRec f (MS list) = MS ( map (\(e, c) -> (f e, c))  list)
+
+
+{-- 
+It is not possible to define an instance of Functor for MSet,
+since the signature of the function should be:
+(a -> b) -> MSet a -> MSet b
+but in order to use map on the MSet we need the signature:
+Eq b = > (a -> b) -> MSet a -> MSet b
+since it could happen that if we have two pairs in the MSet (v_1, c_1) and (v_2, c_2) and
+the function could map both v1 and v2 to v, and at this point we have to merge the two
+pairs in a unique pair (v, c_1 + c_2). Of course in order to do this the type of b support equality
+
+For the implementation, first the mapMSetRec function is called to apply the passed function f
+to all the elements of the MSet without merging duplicate pairs. Then we apply the union to the 
+MSet returned by mapMSetRec and and emply MSet, so that all the duplicate paris are merged
+--}
 mapMSet :: Eq b => (a -> b) -> MSet a -> MSet b
 mapMSet f (MS x) = union (mapMSetRec f (MS x)) (MS [])
--- Assignment2/Exercises_1_2/TestMSet.hs
+++ Assignment2/Exercises_1_2/TestMSet.hs
@@ -2,26 +2,64 @@ import MultiSet
 import System.IO.Unsafe
 import Data.List
 
+{--
+Function to read the content of the auxiliary files and return and IO moand of base 
+type of list of strings.
+I assumed that in the file which is read by the function each line contains
+just one word like in the auxiliary files
+--}
 getStrings :: [Char] -> IO [[Char]]
 getStrings filename = do
   text <- readFile filename
   return (words text)
 
+
+{--
+Function which extract from the IO monad the list of words and for each of them it computes its CIAO
+The unsafePerformIO can be used since the file doesn't change during the execution of the program
+--}
 getCiaoStrings :: [Char] -> [[Char]]
 getCiaoStrings filename = map (\x->sort x) (unsafePerformIO (getStrings filename))
 
+
+
+{--
+Function which starting from an empty MSet add the CIAO of each word read from 
+the file corresponding to the passed file name 
+--}
 readMSet :: [Char] -> MSet [Char]
 readMSet filename = foldr (\x mset -> add mset x) (MS []) (getCiaoStrings filename)
 
+
+{--
+Function used to produce a line of the file while writing an MSet,
+with the format: <element> - <multiplicity> 
+--}
 getFileLine :: (Show a, Show b) => a -> b -> [Char]
 getFileLine val mult = "<" ++ (show val) ++ "> - <" ++ (show mult) ++ ">\n"
 
+
+{--
+Function which produces the text to write on the file, concatenating the strings that
+reprsente the elements of the MSet
+--}
 getText :: (Foldable l, Show a, Show b) => l (a, b) -> [Char]
 getText words = foldr (\(val, mult) text ->  text ++ (getFileLine val mult) ) "" words
 
+
+{--
+Function which takes in input an MSet of strings and writes it in a file
+where each line represent an element of the MSet
+--}
 writeMSet :: MSet [Char] -> [Char] -> IO ()
 writeMSet (MS x) filename = writeFile filename (getText x)
 
+
+{--
+Function which take as input four MSet of strings: m1, m2, m3, m4
+It write m1 and m4 as files only if m1 is different from m4, but they contain the
+same elements, and if the union of m2 and m3 is equal to m1
+--}
 readFiles :: MSet [Char] -> MSet [Char] -> MSet [Char] -> MSet [Char] -> IO ()
 readFiles m1 m2 m3 m4 = if ( m1 /= m4 && (elems m1) == (elems m4) )
   then if ( m1 == (MultiSet.union m2 m3) )
@@ -34,6 +72,9 @@ readFiles m1 m2 m3 m4 = if ( m1 /= m4 && (elems m1) == (elems m4) )
     return ()
 
 
+{-- 
+Main function to call the readFiles function on the four auxiliary files
+--}
 main :: IO()
 main = do
   readFiles (readMSet "./aux_files/anagram.txt") (readMSet "./aux_files/anagram-s1.txt") (readMSet "./aux_files/anagram-s2.txt") (readMSet "./aux_files/margana2.txt")
